1e9dc61aa4b2b8182f98e6e1ef101c9df0d6b0ac
Talha Gergin <talhagergin6@gmail.com>
2026-08-27 13:25:19 +0300

    Initial Commit

---
+++ Rust Block/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Rust Block
+//
+//  Created by Talha Gergin on 27.08.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Rust Block/Rust_BlockApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rust_BlockApp.swift
+//  Rust Block
+//
+//  Created by Talha Gergin on 27.08.2026.
+//
+
+import SwiftUI
+
+@main
+struct Rust_BlockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
